ab0493be577a8ebe31e8659187e23fac364efc4f
rapuncia <keruzu@mail.ru>
2020-07-30 20:05:34 +0300

    Time was added

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+(function(){
+    'use strict';
+    console.log('Is this working?');
+    document.addEventListener('DOMContentLoaded', function(){
+        var c = document.getElementById('current-time');
+        
+        var d = new Date();
+        
+        c.innerHTML = d.toTimeString();
+    });
+    
+    
+    })();
