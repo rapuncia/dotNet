d1c3bf3f1fe99aea3b02e312af0054e95909e438
rapuncia <keruzu@mail.ru>
2020-08-14 15:57:12 +0300

    time and date without style

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -4,7 +4,7 @@
 // Write your JavaScript code.
 (function(){
     'use strict';
-    console.log('Is this working?');
+   /*
     function counter() {
         var c = document.getElementById('current-time');
         var d = new Date();
@@ -19,5 +19,59 @@
        counter();
        setInterval(counter, 50)
     });
-    
+    */
+   document.addEventListener('DOMContentLoaded', function(){
+	var currentTime = document.getElementById('current-time'),
+		currentDate = document.getElementById('current-date');
+	
+	setInterval(function() {
+		var d = new Date();
+	
+		var hours = d.getHours(),
+			minutes = d.getMinutes(),
+			month = formatMonth(d.getMonth()),
+			date = d.getDate(),
+			ampm = 'AM';
+			
+		if (hours > 12) {
+			hours -= 12;
+			ampm = 'PM';
+		} else if (hours === 0) {
+			hours = 12;
+		}
+		
+		if (minutes < 10) {
+			minutes = '0' + minutes;
+		}
+		
+		var sepClass = '';
+		if (d.getSeconds() % 2 === 1) sepClass = 'trans';
+		
+		var sep = '<span class="' + sepClass + '">:</span>';
+	
+		currentTime.innerHTML = hours + sep + minutes + ' ' + ampm;
+		currentDate.textContent = month + ' ' + date;
+	}, 1000);
+	
+	function formatMonth(m) {
+		m = parseInt(m, 10);
+	
+		if (m < 0) {
+			m = 0;
+		} else if (m > 11) {
+			m = 11;
+		}
+		
+	
+		var monthNames = [
+			'January', 'February', 'March',
+			'April', 'May', 'June', 
+			'July', 'August', 'September',
+			'October', 'November', 'December'
+		];
+		
+		return monthNames[m];
+	}
+});
+
     })();
